8e7eb51665073591eefb6a067f551f5c23f134fd
Matej Minar <matej.minar@triad.sk>
2018-07-19 14:23:02 +0200

    setting xcsrf token to jquery headers

--- assets/js/base-components/Form/bootstrap.js
+++ assets/js/base-components/Form/bootstrap.js
@@ -1,5 +1,12 @@
+// This file is here only to ensure backwards compatibility
+
 import Vue from 'vue';
 import jQuery from 'jquery';
 
 window.$ = window.jQuery = jQuery;
 window.Vue = Vue;
+
+let token = document.head.querySelector('meta[name="csrf-token"]');
+if (token) {
+    $.ajaxSetup({headers: {'X-CSRF-TOKEN': token.content}});
+}
