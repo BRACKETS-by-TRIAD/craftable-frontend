b47193da41da3a3d574c2b907c7dc14084aa2dc9
Matej Minar <matej.minar@brackets.sk>
2018-11-22 17:33:53 +0100

    changed paths

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
-export {default as BaseForm} from './assets/js/base-components/Form/BaseForm';
-export {default as BaseUpload} from './assets/js/base-components/Form/BaseUpload';
-export {default as BaseListing} from './assets/js/base-components/Listing/BaseListing';
+export {default as BaseForm} from './base-components/Form/BaseForm';
+export {default as BaseUpload} from './base-components/Form/BaseUpload';
+export {default as BaseListing} from './base-components/Listing/BaseListing';
 
-export {default as Admin} from './assets/js/admin/Admin';
-export {default as Auth} from './assets/js/auth/Form';
-export {default as TranslationForm } from './assets/js/translation/Form';
-export {default as TranslationListing } from './assets/js/translation/Listing';
+export {default as Admin} from './admin/Admin';
+export {default as Auth} from './auth/Form';
+export {default as TranslationForm } from './translation/Form';
+export {default as TranslationListing } from './translation/Listing';
