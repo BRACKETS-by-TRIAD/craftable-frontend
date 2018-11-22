fba983279b9eb5a01ac52098fe61f0d9eaebc07a
Matej Minar <mato7221@gmail.com>
2018-11-22 18:10:39 +0100

    Prepublishing scripts refactor (#7)
    * changed folder structure
    
    * changed paths
    
    * build scripts written
    
    * whitelisted files to publish to npm, new entry file specified
    
    * gitignore edited

--- index.js
+++
@@ -1,8 +0,0 @@
-export {default as BaseForm} from './assets/js/base-components/Form/BaseForm';
-export {default as BaseUpload} from './assets/js/base-components/Form/BaseUpload';
-export {default as BaseListing} from './assets/js/base-components/Listing/BaseListing';
-
-export {default as Admin} from './assets/js/admin/Admin';
-export {default as Auth} from './assets/js/auth/Form';
-export {default as TranslationForm } from './assets/js/translation/Form';
-export {default as TranslationListing } from './assets/js/translation/Listing';
---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+export {default as BaseForm} from './base-components/Form/BaseForm';
+export {default as BaseUpload} from './base-components/Form/BaseUpload';
+export {default as BaseListing} from './base-components/Listing/BaseListing';
+
+export {default as Admin} from './admin/Admin';
+export {default as Auth} from './auth/Form';
+export {default as TranslationForm } from './translation/Form';
+export {default as TranslationListing } from './translation/Listing';
