f3e5ddfcf387fdbdc2d0519d5be59a5610a21eaf
Richard <riso.dominik@gmail.com>
2019-01-30 14:10:02 +0100

    wip

--- src/js/translation/Listing.js
+++ src/js/translation/Listing.js
@@ -1,5 +1,3 @@
-import Vue from 'vue';
-
 import BaseListing from '../base-components/Listing/BaseListing';
 
 Vue.component('translation-listing', {
@@ -12,21 +10,37 @@ Vue.component('translation-listing', {
             default: function() {
                 return 'All groups';
             }
+        },
+        stepcount: {
+            type: Number,
+            default: function(){
+                return 3;
+            }
         }
     },
 
     data(){
         return {
-
+            templateChecked: false,
+            exportLanguage: '',
+            templateLanguage: '',
+            importLanguage: '',
+            file: null,
+            onlyMissing: false,
+            currentstep: 1,
             scanning: false,
-
             filters: {
                 group: null
             },
-
             translationId: null,
             translationDefault: '',
-            translations: {}
+            numberOfSuccessfullyImportedLanguages: 0,
+            numberOfSuccessfullyUpdatedLanguages: 0,
+            numberOfFoundTranslations: 0,
+            numberOfTranslationsToReview: 0,
+            conflicts: null,
+            translations: {},
+            importedFile: null,
         }
     },
 
@@ -34,10 +48,12 @@ Vue.component('translation-listing', {
         filteredGroup() {
             return this.filters.group === null ? this.label : this.filters.group;
         },
+        lastStep() {
+            return this.currentstep === this.stepcount;
+        }
     },
 
     methods: {
-
         rescan(url) {
             this.scanning = true;
             axios.post(url)
@@ -63,6 +79,96 @@ Vue.component('translation-listing', {
         editTranslation (item) {
             this.$modal.show('edit-translation', item);
         },
+
+        showImport () {
+            this.$modal.show('import-translation');
+        },
+
+        showExport () {
+            this.$modal.show('export-translation');
+        },
+
+        nextStep(){
+            if(this.currentstep === 1){
+                return this.$validator.validateAll()
+                    .then(result => {
+                        if (!result) {
+                            this.$notify({ type: 'error', title: 'Error!', text: 'The form contains invalid fields.'});
+                            return false;
+                        }
+
+                        let url = '/admin/translations/import';
+                        let formData = new FormData();
+
+                        formData.append('fileImport', this.file);
+                        formData.append('importLanguage', this.importLanguage);
+                        formData.append('onlyMissing', this.onlyMissing);
+
+                        axios.post(url, formData, {
+                            headers: {
+                                'Content-Type': 'multipart/form-data'
+                            }
+                        }).then(response => {
+                            if(response.data.hasOwnProperty('numberOfImportedTranslations') && response.data.hasOwnProperty('numberOfUpdatedTranslations')){
+                                this.currentstep = 3;
+                                this.numberOfSuccessfullyImportedLanguages = response.data.numberOfImportedTranslations;
+                                this.numberOfSuccessfullyUpdatedLanguages = response.data.numberOfUpdatedTranslations;
+                                this.loadData();
+                            } else {
+                                this.currentstep = 2;
+                                this.numberOfFoundTranslations = Object.keys(response.data).length;
+                                this.conflicts = response.data;
+
+                                for(let i = 0; i < this.conflicts.length; i++){
+                                    if(this.conflicts[i].hasOwnProperty('has_conflict')){
+                                        if(this.conflicts[i].has_conflict) {
+                                            this.numberOfTranslationsToReview++;
+                                        }
+                                    }
+                                }
+                            }
+                        }, error => {
+                            this.$notify({ type: 'error', title: 'Error!', text: 'An error has occured.'});
+                        });
+                    });
+            } else if(this.currentstep === 2){
+                return this.$validator.validateAll()
+                    .then(result => {
+                        if (!result) {
+                            this.$notify({ type: 'error', title: 'Error!', text: 'The form contains invalid fields.'});
+                            return false;
+                        }
+
+                        for(let i = 0; i < this.conflicts.length; i++){
+                            if(this.conflicts[i].hasOwnProperty('checkedCurrent')){
+                                if(this.conflicts[i].checkedCurrent) {
+                                    this.conflicts[i][this.importLanguage.toLowerCase()] = this.conflicts[i].current_value;
+                                }
+                            }
+                        }
+
+                        let url = '/admin/translations/import/conflicts';
+                        let data = {
+                            importLanguage: this.importLanguage,
+                            resolved_translations: this.conflicts
+                        };
+
+                        axios.post(url, data).then(response => {
+                            this.currentstep = 3;
+                            this.numberOfSuccessfullyImportedLanguages = response.data.numberOfImportedTranslations;
+                            this.numberOfSuccessfullyUpdatedLanguages = response.data.numberOfUpdatedTranslations;
+                            this.loadData();
+                        }, error => {
+                            this.$notify({ type: 'error', title: 'Error!', text: 'An error has occured.'});
+                        });
+                    });
+            }
+        },
+
+        previousStep(){
+            this.currentstep--;
+        },
+
         beforeModalOpen ({params}) {
             this.translationId = params.id;
             this.translationDefault = params.key;
@@ -84,6 +190,27 @@ Vue.component('translation-listing', {
             }, error => {
                 this.$notify({ type: 'error', title: 'Error!', text: 'An error has occured.'});
             });
+        },
+        onSubmitExport(){
+            return this.$validator.validateAll()
+                .then(result => {
+                    if (!result) {
+                        this.$notify({ type: 'error', title: 'Error!', text: 'The form contains invalid fields.'});
+                        return false;
+                    }
+
+                    let data = {
+                        exportLanguage: this.exportLanguage,
+                        templateLanguage: this.templateLanguage
+                    };
+                    let url = '/admin/translations/export?' + $.param(data);
+                    this.$modal.hide('edit-translation');
+                    window.location = url;
+                });
+        },
+        handleImportFileUpload(e){
+            this.file = this.$refs.file.files[0];
+            this.importedFile = e.target.files[0];
         }
     }
 });
